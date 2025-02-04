80a06bf8a71b10980834d207393c16a0fe1bd79d
savperera369 <saviru2002@outlook.com>
2025-02-04 01:10:38 -0500

    create home screen and map mode and trim mode pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import ExpandButton from "@/components/ExpandButton";
 
+const mapModeDescription = "This is Map Mode. In map mode, users can view a variety of different settings.";
+const trimModeDescription = "This is Trim Mode";
 
 export default function Home() {
   return (
     <div className="px-4 py-4 flex items-center justify-center min-h-screen">
-      <div className="bg-gray-100 rounded-md shadow-md px-8 py-8 flex flex-col gap-4 items-center">
+      <div className="w-1/2 bg-gray-100 rounded-md shadow-md px-8 py-8 flex flex-col gap-2 items-center">
           <p className="text-xl font-semibold">Select a Mode</p>
-          <div className="flex items-center justify-center gap-x-4">
-              <button className="px-4 py-4 rounded-lg text-sm bg-blue-500 text-white transition-all hover:bg-blue-700">MAP Mode</button>
-              <button className="px-4 py-4 rounded-lg text-sm bg-blue-500 text-white transition-all hover:bg-blue-700">TRIM Mode</button>
+          <div className="w-full flex flex-col items-center justify-center gap-y-1">
+              <ExpandButton buttonTitle="MAP Mode" content={mapModeDescription} href={"/map"}/>
+              <ExpandButton buttonTitle="Trim Mode" content={trimModeDescription} href={"/trim"}/>
           </div>
       </div>
     </div>
---
+++ src/components/ExpandButton.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { useState } from "react";
+import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import Link from "next/link";
+
+type Props = {
+    buttonTitle: string,
+    content: string,
+    href: string,
+}
+
+const ExpandButton = ({ buttonTitle, content, href } : Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const icon = isOpen ? <IoIosRemove className="h-6 w-6"/> : <IoIosAdd className="h-6 w-6" />
+
+    const togglePanel = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="w-full border-none flex flex-col items-center justify-center p-4">
+            <button className="w-1/2 flex items-center justify-center gap-x-1 px-4 py-4 rounded-lg text-md font-medium bg-blue-500 text-white transition-all hover:bg-blue-700"
+                    onClick={togglePanel}
+            >
+                { icon }
+                { buttonTitle }
+            </button>
+            {isOpen ? (
+                <div className="mt-4 p-4 h-full flex flex-col items-center gap-y-2 border border-dashed rounded-md border-black">
+                    <p className="text-sm font-medium">
+                        {content}
+                    </p>
+                    <Link className="text-sm text-sky-500 transition-all hover:underline hover:underline-offset-2" href={href}>See More</Link>
+                </div>
+            ) : null}
+        </div>
+    );
+};
+
+export default ExpandButton;
